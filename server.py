ffe189d5a771b082ab802f65c776021e4c4256b0
Doan Tuan <tuannd@hblab.vn>
2018-11-05 07:48:00 +0700

    update path

--- server.py
+++ server.py
@@ -38,10 +38,10 @@ if __name__ == "__main__":
     sc = init_spark_context()
 
     #for run on server
-    dataset_path = os.path.join('datasets')
+    #dataset_path = os.path.join('datasets')
 
     # for run on local
-    #dataset_path = os.path.join('./RecommendVnsDoc')
+    dataset_path = os.path.join('./Recommend_vnsdoc')
     app = create_app(sc, dataset_path)
 
     # start web server
